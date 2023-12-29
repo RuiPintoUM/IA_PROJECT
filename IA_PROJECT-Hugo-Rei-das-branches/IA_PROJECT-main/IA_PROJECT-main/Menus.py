96dbdfb44d395d5315c804acd4fae5fff7eac9c2
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2023-12-29 02:14:26 +0000

    Update Caminho + estafeta.Classificacao

---
+++ IA_PROJECT-Hugo-Rei-das-branches/IA_PROJECT-main/IA_PROJECT-main/Menus.py
@@ -0,0 +1,180 @@
+from Grafo import Graph
+from Mapa import fill_graph, heuristicaCombustivel, heuristicaTemporais, heuristicaTransito
+from Sistema import Sistema
+
+def menuRankings(sistema, nome):
+    print("1 - Top 5 ranking estafetas com mais entregas efetuadas") # incluir posição atual do estafeta no ranking
+    print("2 - Top 5 ranking estafetas com mais entregas ecológicas")
+    print("3 - Top 5 ranking estafetas com melhor rating")
+    print("4 - Sair")
+
+    user_input = int(input("Introduza a sua opcao-> "))
+    match user_input:
+        case 1:
+            print()
+        case 2:
+            print()
+        case 3:
+            print()
+        case 4:
+            print(menuEstafeta(sistema, nome))
+
+def menuEncomendasEstafeta(sistema, nome):
+    print(sistema.mostrarListaEncomendas(nome))
+    print("--- Escolha o modo de entrega ---")
+    print("1 - Caminho mais rápido")
+    print("2 - Caminho mais ecológico")
+    print("3 - Sair")
+
+    user_input = int(input("Introduza a sua opcao-> "))
+    match user_input:
+        case 1:
+            print()
+        case 2:
+            print()
+        case 3:
+            print(menuEstafeta(sistema, nome))
+
+def menuTrabalhos(sistema):
+    print("\n--- Menu Trabalhos ---\n")
+    print(" 1 - Trabalho 1")
+    print("     Encomenda para entregar em Elvas")
+    print("     Encomenda para entregar em Montemor")
+    print("     Encomenda para entregar em Estremoz")
+    print(" 2 - Trabalho 2")
+    print("     Encomenda para entregar em Vendasnovas")
+    print("     Encomenda para entregar em Redondo")
+    print("     Encomenda para entregar em Moita")
+    print(" 3 - Trabalho 3")
+    print("     Encomenda para entregar em Arraiolos")
+    print("     Encomenda para entregar em Borba")
+    print("     Encomenda para entregar em Palmela")
+
+    user_input = int(input("Introduza a sua opcao - "))
+
+    match user_input:
+        case 1:
+            sistema.executaTrabalho("elvas", "montemor", "estremoz")
+        case 2:
+            sistema.executaTrabalho("vendasnovas", "redondo", "moita")
+        case 3:
+            sistema.executaTrabalho("arraiolos", "borba", "palmela")
+
+def menuEstafeta(sistema, nome):
+    print("\n--- Menu Estafeta ---\n")
+    print("1 - Verificar encomendas associadads")
+    print("2 - Rankings")
+    print("3 - Alterar Status")
+    print("4 - Trabalhos para Efetuar")
+    print("5 - Sair")
+
+    user_input = int(input("Introduza a sua opcao-> "))
+
+    match user_input:
+        case 1:
+            menuEncomendasEstafeta(sistema, nome)
+        case 2:
+            menuRankings(sistema, nome)
+        case 3:
+            print("por fazer")
+        case 4:
+            menuTrabalhos(sistema)
+        case 5:
+            menuEstafetaLogin(sistema)
+
+def menuEstafetaLogin(sistema):
+    print("\n--- Menu Login Estafeta ---\n")
+    print("1 - Login")
+    print("2 - Registar novo estafeta")
+    print("3 - Sair")
+
+    user_input = int(input("Introduza a sua opcao-> "))
+    match user_input:
+        case 1:
+            nome_estafeta = input("Introduza o nome do estafeta-> ")
+            if sistema.loginEstafeta(nome_estafeta):
+                menuEstafeta(sistema, nome_estafeta)
+            else:
+                print("Estafeta não registado")
+                menuEstafetaLogin(sistema)
+        case 2:
+            nome_estafeta = input("Introduza o nome do estafeta-> ")
+            sistema.adicionarEstafeta(nome_estafeta, 0)
+            menuEstafetaLogin(sistema)
+        case 3:
+            print("AGUEM ESCREVA AQUI O CÓDIGO, HUGO SECA NAO SABE FAZER MENUS") # -------------------------------------------------------------
+
+def menuCliente(sistema, g):
+    print("\n--- Menu Cliente ---\n")
+    peso = input("Peso da Encomenda: ")
+    volume = input("Volume da Encomenda: ")
+    tempoPedido = input("Tempo em que quer receber a Encomenda (0 se não tiver preferência): ")
+    local = input("Local onde deseja receber a Encomenda: ")
+
+    resultadoCaminho = sistema.calculaMelhorCaminho(g, local)
+
+    if resultadoCaminho is None:
+        print("O sistema não encontrou caminho para o local desejado.")
+        Menus(sistema)
+    else:
+        (caminho, distancia) = resultadoCaminho
+        encomenda = sistema.criaEncomenda(local, peso, volume, tempoPedido, distancia)
+
+        if encomenda.veiculo == "na":
+            print("Encomenda não pode ser entregue no tempo pedido.")
+        elif encomenda.estafeta == "na":
+            print("Não há estafetas disponíveis no momento.")
+        else:
+            confirmacao = input(f"O preço da encomenda é {encomenda.preco}. Deseja aceitar? (S ou N): ")
+
+            if confirmacao == "S".lower():
+                pass
+            else:
+                sistema.removeEncomenda(encomenda)
+                Menus(sistema)
+
+            print("...Encomenda a ser feita no momento...")
+
+            sistema.respostaPosEncomenda(encomenda, caminho)
+
+            avaliacao = input("Digite a avaliacao do gajo (0 a 5): ")
+
+            sistema.atribuiAvaliacao(encomenda.estafeta, avaliacao)
+
+            print(f"Somaclassificaçoes: {encomenda.estafeta.somaClassificacoes}.")
+
+            print(f"A avaliação média de {encomenda.estafeta.nome} é {sistema.mediaEstafeta(encomenda.estafeta)}.")
+
+    Menus(sistema)
+
+def Menus(sistema):
+    print("\n--- Menu Principal ---\n")
+    g = Graph()
+    fill_graph(g)
+    #sistema = Sistema()
+
+    saida = 1
+
+    while saida != 0:
+        print("1 - Interface Cliente")
+        print("2 - Interface Estafeta")
+        print("0 - Sair")
+
+        saida = int(input("Introduza a sua opcao - "))
+
+        if saida == 0:
+
+            print("saindo.......")
+
+        elif saida == 1:  # interface cliente
+
+            menuCliente(sistema, g)
+
+        elif saida == 2:  # interface estafeta
+
+            menuEstafetaLogin(sistema)
+
+        else:
+            print("you didn't add anything")
+            Menus(sistema)
+
---
+++ IA_PROJECT-Hugo-Rei-das-branches/IA_PROJECT-main/IA_PROJECT-main/Sistema.py
@@ -0,0 +1,146 @@
+from Grafo import Graph
+from Mapa import fill_graph, heuristicaCombustivel, heuristicaTemporais, heuristicaTransito
+from Encomenda import Encomenda
+from Estafeta import Estafeta
+import time
+
+class Sistema:
+
+    def __init__(self):     #  construtor do sistema"
+        self.mapEstafetas = {}  # Dicionário para armazenar estafetas
+        self.mapEncomendas = {}  # Dicionário para armazenar encomendas
+
+    def respostaPosEncomenda(self,encomenda, caminho):
+        print("Caminho: ")
+        for node in caminho:
+            print(str(node))
+        print(f"Encomenda demorou {encomenda.tempoReal} horas a ser entregue.")
+        print(f"Encomenda foi entregue de {encomenda.veiculo}.")
+        print(f"A distancia percorrida foi de {encomenda.distancia}km.")
+
+    def removeEncomenda(self, encomenda):
+        if encomenda.id in self.mapEncomendas:
+            del self.mapEncomendas[encomenda.id]
+
+        for estafeta in self.mapEstafetas.values():
+            if encomenda.id in estafeta.encomenda_ids:
+                estafeta.encomenda_ids.remove(encomenda.id)
+
+        encomenda.estafeta.status = 0
+
+    def printanomes(self):
+        for estafeta_id, estafeta in self.mapEstafetas.items():
+            print(estafeta.nome)
+
+    def executaTrabalho(self, local1, local2, local3):
+        pass
+
+    def criaEncomenda(self, local, peso, volume, tempoPedido, distancia):
+        # Gera um novo ID para a encomenda
+        if not self.mapEncomendas:
+            id = 1
+        else:
+            id = max(self.mapEncomendas.keys()) + 1
+
+        enc = Encomenda(id, local, peso, volume, tempoPedido, distancia)
+
+        if enc.tempoReal != "na":
+            enc.estafeta = self.atribuiEncomenda(id)
+
+        self.mapEncomendas[enc.id] = enc
+        return enc
+
+    def atribuiEncomenda(self, idEncomenda):
+
+        print("À Procura do teu Estafeta.")
+
+        time.sleep(1) #feature riso só para mostrar que tamos à procura dele
+
+        for nome_estafeta, estafeta in self.mapEstafetas.items():
+            if estafeta.status == 0:
+                print("Encontramos o teu Estafeta")
+                print(estafeta.nome)
+                estafeta.status = 1
+                estafeta.addEncomenda(idEncomenda)
+                return estafeta
+
+        return "na"
+
+    def atribuiAvaliacao(self, estafeta, avaliacao):
+        estafeta.somaClassificacoes += int(avaliacao)
+        estafeta.status = 0
+
+    def mediaEstafeta(self, estafeta):
+        print(estafeta.somaClassificacoes)
+        print("/")
+        print(len(estafeta.encomenda_ids))
+        return estafeta.somaClassificacoes / len(estafeta.encomenda_ids)
+
+    def calculaMelhorCaminho(self, g, local):
+        result_BFS = g.procura_BFS("lisboa", local)
+        result_DFS = g.procura_DFS("lisboa", local)
+
+        if result_BFS is None and result_DFS is None:
+            return None
+        elif result_BFS is not None and result_DFS is None:
+            (path1, custo1) = result_BFS
+            return (path1, custo1)
+        elif result_DFS is not None and result_BFS is None:
+            (path2, custo2) = result_DFS
+            return (path2, custo2)
+        else:
+            (path1, custo1) = result_BFS
+            (path2, custo2) = result_DFS
+
+            custoMin = min(custo1, custo2)
+
+            if custo1 == custoMin:
+                melhorCaminho = path1
+            elif custo2 == custoMin:
+                melhorCaminho = path2
+
+            print("Melhor Caminho:", melhorCaminho)
+            print("Custo Mínimo:", custoMin)
+
+            return (melhorCaminho, custoMin)
+
+    def adicionarEstafeta(self, nome_estafeta, status):
+        estafeta = Estafeta(nome_estafeta, status)
+        self.mapEstafetas[nome_estafeta] = estafeta
+
+    def loginEstafeta(self, nome):
+        if nome in self.mapEstafetas:
+            return True
+        else:
+            return False
+        
+    def mostrarListaEncomendas(self, nome):
+        estafeta = self.mapEstafetas.get(nome)
+        
+        return estafeta.getEncomendas()
+
+    def iniciaEstafetas(self):
+        self.adicionarEstafeta("Jefferson", 1)
+        self.adicionarEstafeta("Walson", 1)
+        self.adicionarEstafeta("Valter", 0)
+        self.adicionarEstafeta("Flipe", 0)
+
+    # --- Queries ---
+
+    def top_ranking_entregas(self):
+        sorted_estafetas = sorted(self.estafetas.items(), key=estafeta.encomenda_ids.len , reverse=True)
+        print(f"Top 5 estafetas com mais entregas efetuadas:")
+        for i, (estafeta, dados) in enumerate(sorted_estafetas[:5], 1):
+            print(f"{i}. {estafeta} - Entregas: {dados['entregas']}")
+
+    def top_ranking_ecologicas(self, n=5):
+        sorted_estafetas = sorted(self.estafetas.items(), key=lambda x: x[1]['entregas_ecologicas'], reverse=True)
+        print(f"Top {n} estafetas com mais entregas ecológicas:")
+        for i, (estafeta, dados) in enumerate(sorted_estafetas[:n], start=1):
+            print(f"{i}. {estafeta} - Entregas Ecológicas: {dados['entregas_ecologicas']}")
+
+    def top_ranking_rating(self, n=5):
+        sorted_estafetas = sorted(self.estafetas.items(), key=lambda x: x[1]['rating'], reverse=True)
+        print(f"Top {n} estafetas com melhor rating:")
+        for i, (estafeta, dados) in enumerate(sorted_estafetas[:n], start=1):
+            print(f"{i}. {estafeta} - Rating: {dados['rating']}")
