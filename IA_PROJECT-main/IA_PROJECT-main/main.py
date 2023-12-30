4144f04b8fae496d4c0fd258d3f15aed4bab5a0e
Hugo Dias <116220916+hugodiasUM@users.noreply.github.com>
2023-12-30 14:26:07 +0000

    Delete IA_PROJECT-main/IA_PROJECT-main directory

--- IA_PROJECT-main/IA_PROJECT-main/main.py
+++
@@ -1,9 +0,0 @@
-from Grafo import Graph
-from Mapa import fill_graph, heuristicaCombustivel, heuristicaTemporais, heuristicaTransito
-from Menus import Menus
-
-def main():
-    Menus()
-
-if __name__ == "__main__":
-    main()
